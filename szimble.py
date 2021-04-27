fc2de7e8e8d4ed81cdd46924f2cb96f43262e273
Kai Käpölä <kai.kapola@iki.fi>
2021-04-27 19:54:01 +0300

    Run

---
+++ szimble.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Kimble clone
+#
+# Copyright (C) 2021 Kai Käpölä
+#
+
+
+# Time-stamp: <2021-02-28 08:04:42>
+import logging
+import sys
+import math
+import yaml
+import numpy
+import os
+import random
+from collections import defaultdict
+
+# Start logging before other libraries
+# import argparse
+# from apscheduler.schedulers.blocking import BlockingScheduler
+# import csv
+import pprint
+# import time
+# import docstrings
+
+# Logging
+
+
+class ListHandler(logging.Handler):  # Inherit from logging.Handler
+    def __init__(self, log_list):
+        # run the regular Handler __init__
+        logging.Handler.__init__(self)
+        # Our custom argument
+        self.log_list = log_list
+
+    def emit(self, record):
+        # record.message is the log message
+        self.log_list.append(record.msg)
+
+
+logging.basicConfig(format='%(asctime)s|%(levelname)s|%(filename)s|%(funcName)s|%(lineno)d|%(message)s',
+                    filename='./log/main.log', level=logging.DEBUG)
+
+hud_console_log = []
+hud_console = ListHandler(hud_console_log)
+hud_console.setLevel(logging.INFO)
+
+hud_formatter = logging.Formatter('%(asctime)s|%(message)s')
+hud_console.setFormatter(hud_formatter)
+
+logging.getLogger('').addHandler(hud_console)
+
+log = logging.getLogger()
+
+
+HOME_FOLDER = os.path.dirname(os.path.abspath(__file__))
+
+
+
+def main():
+
+
+    log.info("===========================================")
+    log.info("START")
+
+    log.info("Game is runnning. Press ESC to stop.")
+
+
+    log.info("DONE")
+
+
+if __name__ == "__main__":
+    main()
