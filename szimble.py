aeaedcd0ae94650784c9a6043c36a74d327bbb94
Kai Käpölä <kai.kapola@iki.fi>
2021-04-28 11:45:16 +0300

    Run

--- szimble.py
+++ szimble.py
@@ -111,7 +111,7 @@ class Board():
         for p in self.pegs:
             print(p)
 
-        print (self.pegs.get_player_peg_locations(0))
+        print (Peg.get_player_peg_locations(0))
 
 class Szimble():
     """docstring for ."""
