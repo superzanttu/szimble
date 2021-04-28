15420081796bee9ea0981ba072c2712dd02be88d
Kai Käpölä <kai.kapola@iki.fi>
2021-04-28 12:28:13 +0300

    Run

--- szimble.py
+++ szimble.py
@@ -87,7 +87,7 @@ class Peg():
 
     @classmethod
     def set_player_peg_position(cls,player,id,location):
-        print(cls.peg_instances)
+        pprint.pprint(cls.peg_instances)
         for i in cls.peg_instances:
             if i.player == player and i.id == id:
                 i.location = location
