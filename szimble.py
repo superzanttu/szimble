0f86fedc3f3c1174110479fe581455b46bb67b65
Kai Käpölä <kai.kapola@iki.fi>
2021-05-09 23:39:33 +0300

    Run

--- szimble.py
+++ szimble.py
@@ -60,94 +60,6 @@ logging.getLogger('').addHandler(hud_console)
 log = logging.getLogger()
 
 
-class Old_Peg():
-
-    peg_instances = []
-
-    def __init__(self, player, id):
-        self.player = player
-        self.id = id
-        self.location = 0
-        self.__class__.peg_instances.append(self)
-
-    def __str__(self):
-        return ("Player %s peg %s is at location %s" % (self.player, self.id, self.location))
-
-    def move_peg_to_start(self):
-        print("Moving player %s peg %s to start position" % (self.player, self.id))
-        self.location = 100+self.player*100+self.id
-
-    @classmethod
-    def get_player_pegs(cls,player):
-        pegs_data= []
-        for i in cls.peg_instances:
-            if i.player == player:
-                pegs_data.append([i.player,i.id,i.location])
-        return(pegs_data)
-
-    @classmethod
-    def set_player_peg_position(cls,player,id,location):
-        for i in cls.peg_instances:
-            if i.player == player and i.id == id:
-                i.location = location
-                break
-
-
-class Old_Board():
-    """
-    Slots 0...27 as circle
-
-    Slot definitions
-            Enter  Exit  Start    Goal
-    0 Blue      0    27  100..103 110..113
-    1 Yellow    7     6  200..203 210..213
-    2 Green    14    13  300..303 310..313
-    3 Red      21    20  400..403 410..413
-    """
-
-    def __init__(self):
-        self.pegs = []
-        for p in range(0,4):
-            for c in range(0,4):
-                self.pegs.append(Peg(p,c))
-
-    def setup(self):
-        print("Setup board")
-        for s in self.pegs:
-            s.move_peg_to_start()
-
-
-
-    def print(self):
-        print("Board status")
-
-        board = ["--"]*28
-
-        for player in range(0,4):
-            peg_data = Peg.get_player_pegs(player)
-            print ("Player %s pegs:" % (player),peg_data)
-            for peg in peg_data:
-
-                for slot in range(0,28):
-                    #print (slot,peg[2])
-                    if slot == peg[2]:
-                        print("HIT")
-                        board[slot]="%s%s" % (peg[0], peg[1])
-
-        print(board)
-
-
-class Old_Szimble():
-    """docstring for ."""
-
-    def __init__(self):
-        self.active_player = 0
-        self.board = Board()
-        self.board.setup()
-        self.board.print()
-        Peg.set_player_peg_position(0,1,20)
-        self.board.print()
-
 class Player():
 
     def __init__(self, player_name):
@@ -184,6 +96,9 @@ class Player():
     def play(self):
         dice = random.randrange(1,6)
         print("Dice: %s " % dice)
+
+
+
 
 def main():
 
