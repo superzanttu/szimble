c8f3c4e9490d4732c7dc95c4dce0d71d134276d1
Kai Käpölä <kai.kapola@iki.fi>
2021-05-10 00:24:44 +0300

    Run

--- szimble.py
+++ szimble.py
@@ -130,7 +130,7 @@ class Player():
         action['M1'] = 0 # Move peg 2
         action['M2'] = 0 # Move peg 3s
         action['M3'] = 0 # Move peg 4
-        action['NONE'] = 1 # Don't know what to do
+        action['N0'] = 1 # Don't know what to do
 
 
 
