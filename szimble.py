ca3f22e0123a71cbb2fc7aa06d1de699d1a11877
Kai Käpölä <kai.kapola@iki.fi>
2021-05-11 11:01:41 +0300

    Run

--- szimble.py
+++ szimble.py
@@ -288,7 +288,7 @@ def main():
     log.info("Game is runnning.")
 
     player = Player("Red")
-    player.set_enemy_pegs_location([7,11,23,29])
+    player.set_enemy_pegs_location(1,[7,11,23,29])
 
     player.status()
 
