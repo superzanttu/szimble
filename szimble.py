c29a2511143eaf40069706f13d6fa62916a2e256
Kai Käpölä <kai.kapola@iki.fi>
2021-05-11 12:29:26 +0300

    Run

--- szimble.py
+++ szimble.py
@@ -269,7 +269,7 @@ class Player():
                 if self.slots[target_slot] > 9: # Enemy peg ids are over 9
                     rule_score["X%s" % id] += 90
                     rule_target_slot["X%s" % id] = target_slot
-                    #print ("...enter game with score %s" % (rule_score["E%s" % id]), end=" ")
+                    print ("...eat enemy peg from  game with score %s" % (target_slot, rule_score["E%s" % id]), end=" ")
 
             # Move peg X to goal
             if self.pegs_location[id] < self.slot_goal1 \
