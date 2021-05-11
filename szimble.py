7dc94170602d6dc3cd266330467ada722694bfc2
Kai Käpölä <kai.kapola@iki.fi>
2021-05-11 11:32:27 +0300

    Run

--- szimble.py
+++ szimble.py
@@ -89,7 +89,7 @@ class Player():
         print (id, pegs_location)
 
         for i in range(self.slot_enter, self.slot_goal1):
-            if self.slots[i] >9 :
+            if int(self.slots[i]) > 9 :
                 self.slots[i] = None
 
 
