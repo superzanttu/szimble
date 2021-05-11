9527f1f5a2ed938de3461890d729022afe745cff
Kai Käpölä <kai.kapola@iki.fi>
2021-05-11 11:42:17 +0300

    Run

--- szimble.py
+++ szimble.py
@@ -91,7 +91,7 @@ class Player():
         for i in range(self.slot_enter, self.slot_goal1):
             if self.slots[i]!=None:
                 p = self.slots[i] % 10
-                print (p)
+                print (id,p)
                 self.slots[i] = None
 
 
