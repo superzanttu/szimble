b44f049ae9a0377e31b359402e662d599d1975ee
Kai Käpölä <kai.kapola@iki.fi>
2021-05-12 12:18:12 +0300

    Run

--- szimble.py
+++ szimble.py
@@ -116,7 +116,7 @@ class Board():
             else:
                 row += "%s%s" % (cls.peg_id[i],cls.peg_owner_id[i])
 
-            j = 34 - i
+            j = 33 - i
 
             if cls.peg_id[j] == None:
                 row =".." + row
