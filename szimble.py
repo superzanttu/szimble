740efb92cbc8987335861be62eb45ffaff053bb8
Kai Käpölä <kai.kapola@iki.fi>
2021-05-12 10:39:07 +0300

    Run

--- szimble.py
+++ szimble.py
@@ -95,7 +95,7 @@ class Player():
         self.status_winner = False
         self.status_peg_eaten = None
 
-        print(self.board)
+        print(self.board.slots)
 
         # All pegs in start
         self.slots[0] = 0
